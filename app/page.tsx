d65d325597003893e51b5b389fe298a510375ca9
lunar-arch <barcroft.j.nathan@gmail.com>
2026-02-06 08:17:07 -0600

    fix: update import paths for page and layout validation
    - Adjusted import paths in validator.ts for page and layout validation to reflect the correct relative paths.
    - Added a new utility function `cn` in utils.tsx for merging class names using clsx and tailwind-merge.

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,142 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import clsx, { ClassValue } from 'clsx';
+import { motion } from 'framer-motion';
+import { twMerge } from 'tailwind-merge';
+import { useState } from 'react';
+import { cn } from '@/lib/utils'
+import { Plus, Check, X,} from 'lucide-react';
+
+export default function HomePage() {
+  const [direction, setDirection] = useState("col");
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <main className={cn('bg-background w-full min-h-screen overflow-y-auto px-12 py-8 pt-20 flex', direction == "col" ? "flex-col" : "flex-row" )}>
+      <Header />
+      <TaskBlock />
+    </main>
+  );
+}
+
+export function Header() {
+  return (
+    <header className={cn('bg-background shadow-sm fixed inset-x-0 top-0 h-14 py-2 px-4 flex flex-row gap-2 items-center justify-between')}>
+
+    </header>
+  );
+}
+
+export function TaskBlock() {return (
+    <div className={cn('grid grid-cols-[1.75rem_1fr] gap-2 w-full border-t-3 border-border py-2')}>
+      <div className={cn('grid grid-cols-subgrid col-span-2 gap-2 -mx-2 px-2 py-1.5 rounded-lg hover:bg-secondary duration-200 transition-colors cursor-pointer items-center')}>
+        <Plus />
+        <h2 className={cn('text-lg font-semibold text-text')}>Task Block</h2>
+      </div>
+      <ul className={cn('grid grid-cols-subgrid col-span-2 px-2')}>
+        <Task title="Task name" dueDate='tomorrow' completed={false} />
+      </ul>
     </div>
   );
 }
+
+type TaskProps = {
+  title: string,
+  description?: string,
+  dueDate?: string,
+  tags?: TagProps[];
+  completed?: boolean | undefined;
+}
+
+export function Task(props: TaskProps) {
+  const [isCompleted, setIsCompleted] = useState(props.completed ?? false);
+  return (
+    <li className={cn('grid col-span-2 grid-cols-subgrid gap-2 items-center')}>
+      <label className={cn('peer/check-container w-full aspect-square cursor-pointer border-3 border-border rounded-full overflow-hidden')}>
+        <input type="checkbox" className={cn('peer/check-html sr-only')} />
+        <span className={cn('flex group/check-span items-center justify-center w-full h-full peer-checked/check-html:bg-blue-400 duration-175 transition-colors')}>
+          <Check size={12} className={cn('text-text-soft opacity-0 group-hover/check-span:opacity-100 transition-opacity duration-175')}/>
+        </span>
+      </label>
+      <div className={cn('shadow-sm group transition-colors ease-in-out overflow-hidden duration-300 w-full cursor-pointer flex flex-col px-3 py-2 gap-2 rounded-xl border-3 border-border hover:bg-foreground hover:shadow-md peer-hover/check-container:bg-foreground decoration-text-soft')}>
+        <div className={cn('w-full flex flex-row justify-between items-center h-6')}>
+          <h3 className={cn('font-bold text-text')}>{props.title}</h3>
+          <span className={cn('text-xs text-text-soft')}>{props.dueDate}</span>
+        </div>
+        <div className={cn('relative w-full after:w-8 after:absolute after:right-0 after:duration-300 after:transition-colors after:ease-in-out after:inset-y-0 after:bg-linear-to-r after:pointer-events-none after:from-transparent after:to-background group-hover:after:to-foreground')}>
+          {props.tags && props.tags.length > 0 && (
+            <span className={cn('w-full overflow-x-auto no-scrollbar flex flex-row gap-2 pr-6')}>
+              {props.tags.map((tag, i) => (
+                <Tag key={i} {...tag} />
+              ))}
+            </span>
+          )}
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export type TagColors = {
+  border: string;
+  background: string;
+  text: string;
+}
+
+type TagProps = {
+  theme: "success" | "warning" | "destructive" | "info" | "custom";
+  colors?: TagColors;
+  label: string;
+  showX: boolean;
+}
+export function Tag (props: TagProps) {
+  let tagColors : TagColors;
+  if (props.theme === 'custom') {
+    tagColors = props.colors ?? {
+      background: "bg-blue-100",
+      border: "border-blue-400",
+      text: "text-blue-800"
+    };
+  } else {
+    switch (props.theme) {
+      case "success": 
+        tagColors = {
+          background: "bg-green-100",
+          border: "border-green-400",
+          text: "text-green-600"
+        };
+      break;
+      case "warning":
+        tagColors = {
+          background: "bg-yellow-100",
+          border: "border-yellow-400",
+          text: "text-yellow-700"
+        };
+      break;
+      case "destructive":
+        tagColors = {
+          background: "bg-red-100",
+          border: "border-red-400",
+          text: "text-red-600"
+        };
+        break;
+        default:
+        tagColors = {
+          background: "bg-blue-100",
+          border: "border-blue-400",
+          text: "text-blue-800"
+        };
+        break;
+    }
+  }
+  return (
+    <span className={cn('rounded relative leading-none duration-200 group/tag border-2 px-1.5 py-1 text-xs flex flex-row gap-1 items-center justify-center', props.showX && "hover:pr-5.5", tagColors && Object.values(tagColors).join(' ') )}>
+      <p className={cn('-mt-0.5')}>{props.label}</p>
+      { props.showX && (
+        <button className={cn('rounded-full absolute right-1 size-4 opacity-0 group-hover/tag:opacity-100 transition-opacity duration-200 hover:bg-black/10 flex items-center justify-center')}>
+          <X className={cn('')} size={12} />
+        </button>
+      )}
+    </span>
+  );
+}
+
+
